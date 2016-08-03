eec99d5297c342d7d2a2309a0bf0a30e442eb6a8
Christopher Duke <cd.duke@gmail.com>
2016-08-02 23:23:14 -0500

    23 tests passing

---
+++ mirage/models/todo-group.js
@@ -0,0 +1,12 @@
+import { Model } from 'ember-cli-mirage';
+
+export default Model.extend({
+
+});
+
+this.get('/posts');
+this.post('/posts');
+this.get('/posts/:id');
+this.put('/posts/:id');
+this.patch('/posts/:id');
+this.del('/posts/:id');
