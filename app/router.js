e3ea4a5e9c46e42f9ec9f50b942e45f2109fb2c7
Christopher Duke <cd.duke@gmail.com>
2016-08-03 20:35:25 -0500

    ryan I'm comletely lost, please help

--- app/router.js
+++ app/router.js
@@ -6,17 +6,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('todo-group', { path: '/todo-group' }, function() {
-    this.route('new');
-  });
+  this.route('todo-group', { path: '/todo-groups' }, function() {
+  this.route('new');
+});
 
-  this.route('todo-groups', function() {
-    this.route('new');
-
-    this.route(':id', function() {
-      this.route('edit');
-    });
-  });
 });
 
 export default Router;
