39c90df6d09fe1191689a05bb924c6aff8feb90f
Christopher Duke <cd.duke@gmail.com>
2016-08-09 14:07:11 -0500

    style changes/ help from ryan

--- app/todo-groups/detail/controller.js
+++ app/todo-groups/detail/controller.js
@@ -1,5 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  actions: {
+    destroy(value){
+      value.destroyRecord();
+    },
+    editSubmit(group, formValues){
 
+      group.setProperties(formValues);
+      group.save().then(() => {
+
+      this.transitionToRoute('todo-group.index');
+    });
+  }
+}
 });
